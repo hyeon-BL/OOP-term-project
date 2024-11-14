49dfdd9758a7b20416ae78b76b652bd6b57ed4db
jm154 <144792666+jm154@users.noreply.github.com>
2024-11-14 16:55:21 +0900

    Update bank.h

--- bank.h
+++ bank.h
@@ -1,17 +1,52 @@
-// Basic Bank class (will be expanded later)
+// bank.h
+
+#ifndef BANK_H
+#define BANK_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Account;  // Forward declaration of Account class
+
 class Bank {
-private:
-    std::string name;
-    std::vector<std::shared_ptr<Account>> accounts;
+protected:
+    string bankname;
+    Account* accounts[1000]; // Array to hold pointers to Account objects
+    int numaccounts;         // Number of accounts in the bank
+    bool isprimarybank;      // Specifies if the bank is a primary bank
+    int dfee;                // Deposit fee
+    int wfee;                // Withdrawal fee
+    int afee;                // Account transfer fee
+    int cfee;                // Cash transfer fee
 
 public:
-    Bank(std::string bankName) : name(bankName) {}
+    Bank(string bankname);
+    virtual ~Bank();  // Virtual destructor for inheritance
 
-    std::shared_ptr<Account> createAccount(std::string accountNumber, std::string ownerName, double initialBalance = 0) {
-        auto account = std::make_shared<Account>(accountNumber, ownerName, name, initialBalance);
-        accounts.push_back(account);
-        return account;
-    }
+    bool getisprimarybank() const { return isprimarybank; }
+    int getdfee() const { return dfee; }
+    int getwfee() const { return wfee; }
+    int getafee() const { return afee; }
+    int getcfee() const { return cfee; }
 
-    std::string getName() const { return name; }
+    void createaccount();       // Creates a new account
+    void printbankaccount() const;  // Prints account details
+    int* depositcashtoatm(int amount);  // Deposits cash to ATM by denomination
 };
+
+// Derived classes
+class PrimaryBank : public Bank {
+public:
+    PrimaryBank(string name);
+    ~PrimaryBank() override;
+};
+
+class NonPrimaryBank : public Bank {
+public:
+    NonPrimaryBank(string name);
+    ~NonPrimaryBank() override;
+};
+
+#endif // BANK_H
