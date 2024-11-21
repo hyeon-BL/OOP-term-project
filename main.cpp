462630197ea746e7f4e35f74cf89cb5399ff60d9
jm154 <144792666+jm154@users.noreply.github.com>
2024-11-21 20:06:05 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,25 @@ void runTest(const std::string& testName, std::function<void()> testCase) {
     }
 }
 
+
+//bank, account 연결 확인용
+//수수료 정의해둔거 있으니까 필요하면 말하셈. atm 완성되면 초기 atm에 지폐 넣어두는것도 있긴함.
+int main() {
+    // 은행 생성
+    Bank myBank("DaeguBank");
+
+    // 계좌 생성 
+    myBank.createAccount("Alice", 1001, 1234, 0); // 사용자 이름, 계좌번호, 비밀번호, 계좌금액
+    myBank.createAccount("Bob", 1002, 5678, 0);
+
+    // 초기 계좌 정보 출력
+    cout << "Initial Account Information:" << endl;
+    myBank.printbankaccount();
+
+    return 0;
+}
+
+
 int main() {
     // Setup test environment
     PrimaryBank* primaryBank = new PrimaryBank("TestBank");
