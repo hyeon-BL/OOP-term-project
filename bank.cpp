9fea25c7f27197bfa62b3eaa946587b3245f838c
jm154 <144792666+jm154@users.noreply.github.com>
2024-11-21 19:54:46 +0900

    Update bank.cpp

--- bank.cpp
+++ bank.cpp
@@ -2,7 +2,9 @@
 #include "account.h"
 #include <iostream>
 
-Bank::Bank(std::string name) : bankname(name), numaccounts(0) {
+using namespace std;
+
+Bank::Bank(string name) : bankname(name), numaccounts(0) {
     for (int i = 0; i < 1000; i++) {
         accounts[i] = nullptr;
     }
@@ -14,17 +16,19 @@ Bank::~Bank() {
     }
 }
 
-void Bank::createaccount(std::string name, int number, double balance) {
+void Bank::createaccount(string name, int number, int balance, int password) {
     if (numaccounts < 1000) {
-        accounts[numaccounts] = new Account(name, number, balance, bankname);
+        accounts[numaccounts] = new Account(name, number, password, balance);
+        accounts[numaccounts]->setBankName(bankname);
         numaccounts++;
     }
 }
 
-Account* Bank::createAccount(std::string name, int accountNumber, double initialBalance) {
+Account* Bank::createAccount(string name, int accountNumber, int password, int initialBalance) {
     if (numaccounts >= 1000) return nullptr;
-    
-    accounts[numaccounts] = new Account(name, accountNumber, initialBalance, bankname);
+
+    accounts[numaccounts] = new Account(name, accountNumber, password, initialBalance);
+    accounts[numaccounts]->setBankName(bankname);
     numaccounts++;
     return accounts[numaccounts - 1];
 }
@@ -40,36 +44,9 @@ Account* Bank::getAccount(int accountNumber) const {
 
 void Bank::printbankaccount() const {
     for (int i = 0; i < numaccounts; i++) {
-        std::cout << "Bank: " << bankname 
-             << ", Username: " << accounts[i]->getAccName()
-             << ", Account: " << accounts[i]->getAccNumber()
-             << ", Balance: " << accounts[i]->getBalance() << std::endl;
+        cout << "Bank: " << bankname
+            << ", Username: " << accounts[i]->getAccName()
+            << ", Account: " << accounts[i]->getAccNumber()
+            << ", Balance: " << accounts[i]->getBalance() << endl;
     }
-}
-
-int* Bank::depositcashtoatm(int amount) {
-    int* result = new int[4];
-    int t = 50000;
-    for (int i = 0; i < 4; i++) {
-        result[i] = amount / t;
-        amount %= t;
-        t = (t == 10000) ? t / 2 : t / 5;
-    }
-    return result;
-}
-
-PrimaryBank::PrimaryBank(std::string name) : Bank(name) {
-    isprimarybank = true;
-    dfee = 1000;
-    wfee = 1000;
-    afee = 2000;
-    cfee = 1000;
-}
-
-NonPrimaryBank::NonPrimaryBank(std::string name) : Bank(name) {
-    isprimarybank = false;
-    dfee = 2000;
-    wfee = 2000;
-    afee = 3000;
-    cfee = 1000;
 }
